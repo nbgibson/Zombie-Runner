481cbfc6d726aa393f4d6dab35c166ee1ddb6e86
WarriusBird <nbgibson@crimson.ua.edu>
2017-12-22 00:25:56 -0500

    Keybinds implemented

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+    public AudioClip callSound;
+
+    private bool called = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("CallHeli") && !called)
+        {
+            called = true;
+            print("Heli called.");
+        }
+        else
+        {
+            
+        }
+    }
+}
